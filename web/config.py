5aecf60b3565945839ce7bdc2577da8cb52e65e3
KienNgo2012 <ngokien201294@gmail.com>
2023-11-01 14:00:22 +0700

    commit app

---
+++ web/config.py
@@ -0,0 +1,24 @@
+import os
+
+app_dir = os.path.abspath(os.path.dirname(__file__))
+
+class BaseConfig:
+    DEBUG = True
+    POSTGRES_URL="project3-kiennt-dbsever.postgres.database.azure.com"  #TODO: Update value #DONE
+    POSTGRES_USER="project3kiennt@project3-kiennt-dbsever" #TODO: Update value #DONE
+    POSTGRES_PW="Kazoku1:d"   #TODO: Update value #DONE
+    POSTGRES_DB="techconfdb"   #TODO: Update value #DONE
+    DB_URL = 'postgresql://{user}:{pw}@{url}/{db}'.format(user=POSTGRES_USER,pw=POSTGRES_PW,url=POSTGRES_URL,db=POSTGRES_DB)
+    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI') or DB_URL
+    CONFERENCE_ID = 1
+    SECRET_KEY = 'LWd2tzlprdGHCIPHTd4tp5SBFgDszm'
+    SERVICE_BUS_CONNECTION_STRING ='Endpoint=sb://project3-kiennt-service-bus.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=erdTibGEsMGyLYhmZQX20DvFlsqXs1arf+ASbGQhshw=' #TODO: Update value #DONE
+    SERVICE_BUS_QUEUE_NAME ='notificationqueue'
+    SENDGRID_API_KEY = '' #Configuration not required, required SendGrid Account
+
+class DevelopmentConfig(BaseConfig):
+    DEBUG = True
+
+
+class ProductionConfig(BaseConfig):
+    DEBUG = False
